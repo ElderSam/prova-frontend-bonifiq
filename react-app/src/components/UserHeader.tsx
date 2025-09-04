c03a539847b8231cf374ca227643c9b5c6f3c32c
ElderSam <eldersamuel98@gmail.com>
2025-09-04 14:44:14 -0300

    style: update widget styles and add UserHeader component with CSS

--- react-app/src/components/UserHeader.tsx
+++ react-app/src/components/UserHeader.tsx
@@ -1,12 +1,13 @@
 import { use } from 'react';
 import type { User } from '../App';
+import './UserHeader.css';
 
 export function UserHeader({ userPromise }: { userPromise: Promise<User> }) {
   const user = use(userPromise);
 
   return (
     <div className="user-header">
-      <div className="widget-title">
+      <div className="widget-name">
         <strong>Nome: </strong>
         <span>{user.name}</span>
       </div>
